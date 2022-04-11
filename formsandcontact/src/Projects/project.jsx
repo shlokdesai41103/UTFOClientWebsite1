fa01196febde33e6f6c08cb0595535e91c823e4c
shlokdesai41103 <desaishlok41103@gmail.com>
2022-04-10 20:12:41 -0400

    creating project display

--- formsandcontact/src/Projects/project.jsx
+++ formsandcontact/src/Projects/project.jsx
@@ -5,8 +5,8 @@ const Project = props => {
 
         <div className="projectBody">
             <div className='projectText'>
-                <h3 className='projectTitle'>{props.title}</h3>
-                <h8 className='projectDescription'>{props.description}</h8>
+                <h3 className='projectTitle'>Title</h3>
+                <h8 className='projectDescription'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et tenetur perferendis odit eum, illo ratione aperiam, quisquam officiis veritatis rerum ex, exercitationem excepturi a pariatur dolorum quod recusandae doloribus aspernatur?</h8>
             </div>
             <div className='projectImage'>
                 <imgj src = {props.imgsrc} alt="Image" className ="project-image"/>
